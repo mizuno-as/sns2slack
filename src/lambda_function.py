6e5bc265ed1b9c8658a40ea3de7c803f0469aaed
Hajime MIZUNO <mizuno-as@ubuntu.com>
2017-12-01 14:30:35 +0900

    initial version

---
+++ src/lambda_function.py
@@ -0,0 +1,29 @@
+import json
+import os
+import slackweb
+
+print('Loading function')
+
+def lambda_handler(event, context):
+    url = os.getenv("SLACKURL", None)
+    if url is None: exit(1)
+
+    print("Received event: " + json.dumps(event, indent=2))
+
+    channel = os.getenv("CHANNEL", "#sns")
+    name = os.getenv("NAME", "SNS")
+    slack = slackweb.Slack(url=url)
+    message = event['Records'][0]['Sns']['Message']
+    color = "good"
+
+    slack.notify(channel=channel,
+                 username=name,
+                 attachments=[
+                     {
+                         'color': color,
+                         'text': message,
+                     }
+                 ]
+    )
+
+    return message
